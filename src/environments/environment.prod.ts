01853ab094fb4d5d393132b6c7968934acc62146
Jon Preece <jonpreece@hotmail.co.uk>
2020-03-29 16:19:18 +0100

    chore(release): added firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyACwfVY_GaqRR8R_Q47vKvNtUeqc0iiUhY',
+    authDomain: 'outsideir35jobs-272511.firebaseapp.com',
+    databaseURL: 'https://outsideir35jobs-272511.firebaseio.com',
+    projectId: 'outsideir35jobs-272511',
+    storageBucket: 'outsideir35jobs-272511.appspot.com',
+    messagingSenderId: '300483902598',
+    appId: '1:300483902598:web:bff39a5fcd80ecdf67e507',
+    measurementId: 'G-GS8JK5L18N'
+  }
 };
